19e0adb2d0de098921fa5785cebe34fd28087dbe
soloicesky <604421845@qq.com>
2019-02-06 23:39:41 +0800

    finish parse api of tlv obj"

--- common/lydia.h
+++ common/lydia.h
@@ -6,9 +6,18 @@
 #define lydia_memcpy    memcpy
 #define lydia_memcmp    memcmp
 
-#define CHECK_CONDICTION(cond, expect,msg) do {                                 \
-                                              if (cond != expect) {             \
-                                                  DBG("%s:%d",msg,cond);        \
-                                                  goto out;                     \
-                                              }                                 \
-                                           } while (0)
+#define CHECK_CONDICTION(cond, err, msg) do{                                             \
+                                                if (cond) {                              \
+                                                    DBG("%s:[%d]\r\n", msg, err);        \
+                                                    ret = err;                           \
+                                                    goto out;                            \
+                                                }                                        \
+                                            }while(0)
+
+typedef enum _lydia_err_t{
+    LYDIA_ERR_PARAM = -2019,
+    LYDIA_ERR_OOM,
+}lydia_err_t;
+
+#define LYDIA_FALSE         (0)
+#define LYDIA_TRUE          (!LYDIA_FALSE) 
--- tlv/tlv.c
+++ tlv/tlv.c
@@ -6,21 +6,78 @@
 
 static int32_t _tlv_read_tag(uint32_t *tag, uint8_t **data, uint16_t *len){
     int32_t ret = 0;
+    uint8_t *p_data = *data;
+    uint16_t offset = 0;
 
+    CHECK_CONDICTION(tag == NULL, LYDIA_ERR_PARAM, "err parameter");
+    *tag = 0;
+
+    do
+    {
+        CHECK_CONDICTION(offset>=*len, LYDIA_ERR_OOM, "length over flow");
+        *tag = p_data[offset++];
+
+        if (*tag == 0xFF || *tag == 0x00) {
+            *tag = 0x00;
+            continue;
+        }
+
+        if (LYDIA_TLV_SEE_CONTINUE(*tag)) {
+            do
+            {
+                CHECK_CONDICTION(offset>=*len, LYDIA_ERR_OOM, "length over flow");
+                *tag = (*tag << 8) || p_data[offset++];
+            } while (LYDIA_TLV_MORE_TAG(*tag));
+
+            break;
+        }else
+        {
+            break;
+        }
+    } while (1);
+
+    *data = p_data+offset;
+    *len = *len - offset;
     out:
     return ret;
 }
 
 static int32_t _tlv_read_len(uint16_t *l, uint8_t **data, uint16_t *len){
     int32_t ret = 0;
+    uint8_t *p_data = *data;
+    uint16_t offset = 0;
+    uint8_t size = 0;
+
+    CHECK_CONDICTION(l == NULL, LYDIA_ERR_PARAM, "err parameter");
+    *l = 0;
+
+    *l = p_data[offset++];
+    size = LYDIA_TLV_LEN_SIZE(*l);
+
+    if (size == 1) {
+        goto out;
+    }else
+    {
+        do
+        {
+            CHECK_CONDICTION(offset>=*len, LYDIA_ERR_OOM, "length over flow");
+            *l = (*l << 8) | p_data[offset++];
+        } while (size--);
+    }
 
     out:
+    *data = p_data+offset;
+    *len = *len - offset;
     return ret;
 }
 
-static int32_t _tlv_read_v(uint32_t *tag, uint8_t **data, uint16_t *len){
+static int32_t _tlv_read_v(uint8_t **v, uint16_t v_len, uint8_t **data, uint16_t *len){
     int32_t ret = 0;
 
+    CHECK_CONDICTION((v == NULL) || (v_len > *len), LYDIA_ERR_PARAM, "err parameter");
+    *v = *data;
+    *len += v_len;
+    *data = *data + v_len;
     out:
     return ret;
 }
@@ -30,13 +87,13 @@ static int32_t _tlv_read_obj(tlv_t *tlv, uint8_t **data, uint16_t *len){
 
     lydia_memset(tlv, 0x00, sizeof(tlv_t));
     ret = _tlv_read_tag(&tlv->tag, data, len);
-    CHECK_CONDICTION(ret, 0, "read tag failed");
+    CHECK_CONDICTION(ret, ret, "read tag failed");
 
     ret = _tlv_read_len(&tlv->tag, data, len);
-    CHECK_CONDICTION(ret, 0, "read len failed");
+    CHECK_CONDICTION(ret, ret, "read len failed");
 
     ret = _tlv_read_v(&tlv->tag, data, len);
-    CHECK_CONDICTION(ret, 0, "read v failed");
+    CHECK_CONDICTION(ret, ret, "read v failed");
 
     out:
     return ret;
@@ -45,6 +102,7 @@ static int32_t _tlv_read_obj(tlv_t *tlv, uint8_t **data, uint16_t *len){
 int32_t tlv_read_data(tlv_context_pt *ctx, uint8_t *data, uint16_t len,onReadData on_read_data){
     int32_t ret = 0;
     tlv_t tlv;
+    uint16_t size = len;
 
     if (NULL ==ctx ||
         NULL == data ||
@@ -53,6 +111,13 @@ int32_t tlv_read_data(tlv_context_pt *ctx, uint8_t *data, uint16_t len,onReadDat
         goto out;
     }
 
+    CHECK_CONDICTION((NULL == ctx) || (NULL == data) || (len == 0), LYDIA_ERR_PARAM, "invalid parameters");
+
+    do
+    {
+        ret = tlv_read_data(&tlv, &data, &size, on_read_data);
+        CHECK_CONDICTION(ret, ret, "tlv read data [failed]");
+    } while (size > 0);
 
     out:
     return ret;
